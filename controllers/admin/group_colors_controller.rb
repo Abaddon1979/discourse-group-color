c407d81e9e8cc0922570b5a0c186847c7a66d5a1
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2024-11-16 15:43:42 -0500

    a

--- controllers/admin/group_colors_controller.rb
+++ controllers/admin/group_colors_controller.rb
@@ -1,26 +1,24 @@
 # plugins/discourse-group-color/controllers/admin/group_colors_controller.rb
 
 module ::DiscourseGroupColor
-  module Admin
-    class GroupColorsController < ::Admin::AdminController
-      requires_plugin ::DiscourseGroupColor
+  class ::Admin::GroupColorsController < ::Admin::AdminController
+    requires_plugin 'discourse-group-color'
 
-      def index
-        groups = Group.order(:name)
-        render_serialized(groups, ::DiscourseGroupColor::AdminGroupColorSerializer)
-      end
+    def index
+      groups = Group.order(:name)
+      render_serialized(groups, ::DiscourseGroupColor::AdminGroupColorSerializer)
+    end
 
-      def update
-        group = Group.find(params[:id])
-        group.assign_attributes(
-          color: params[:color],
-          rank: params[:rank].to_i
-        )
-        if group.save
-          render json: success_json
-        else
-          render json: failed_json, status: 422
-        end
+    def update
+      group = Group.find(params[:id])
+      group.assign_attributes(
+        color: params[:group][:color],
+        rank: params[:group][:rank].to_i
+      )
+      if group.save
+        render json: success_json
+      else
+        render json: failed_json, status: 422
       end
     end
   end
--- serializers/admin_group_color_serializer.rb
+++ serializers/admin_group_color_serializer.rb
@@ -1,7 +1,7 @@
 # plugins/discourse-group-color/serializers/admin_group_color_serializer.rb
 
 module ::DiscourseGroupColor
-  class AdminGroupColorSerializer < ApplicationSerializer
+  class AdminGroupColorSerializer < ::ApplicationSerializer
     attributes :id, :name, :color, :rank
   end
 end
